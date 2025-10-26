1f77c8b13c3c07a9313499ef3727a061f6fa0dd3
nguyenkhoinguyenn352-tech <nguyenkhoinguyenn352@gmail.com>
2025-10-26 20:49:43 +0700

    Create list of integers as a parameter.py

---
+++ list of integers as a parameter.py
@@ -0,0 +1,11 @@
+def sum_list(numbers):
+    return sum(numbers)
+
+def main():
+    nums = [3, 7, 2, 8, 4]
+    total = sum_list(nums)
+    print(f"The list is: {nums}")
+    print(f"The sum of the numbers is: {total}")
+
+if __name__ == "__main__":
+    main()
